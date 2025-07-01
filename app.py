da83832aa450df16c57e6346787deabe4e65bec5
Ruchit Chaudhary <YOUR_EMAIL>
2025-07-01 07:41:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import warnings
 from dotenv import load_dotenv
 import os
@@ -315,16 +318,18 @@ class resume_analyzer:
 
 
 class linkedin_scraper:
-
+       
     def webdriver_setup():
-            
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-
-        driver = webdriver.Chrome(options=options)
-        driver.maximize_window()
+        options.binary_location = '/usr/bin/chromium'
+    
+        driver = webdriver.Chrome(
+            service=Service('/usr/bin/chromedriver'),
+            options=options
+        )
         return driver
 
 
